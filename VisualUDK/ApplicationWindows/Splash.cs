2a783eac770396d3153ac4624a5327d7977e6eeb
Kyle Harrison <silent.coyote1@gmail.com>
2012-03-08 12:29:47 -0800

    switched landing web page to bitbucket wiki

--- VisualUDK/ApplicationWindows/Splash.cs
+++ VisualUDK/ApplicationWindows/Splash.cs
@@ -23,7 +23,7 @@ namespace VisualUDK
 
         private void Splash_Load(object sender, EventArgs e)
         {
-            SplashBrowser.Url = new Uri( "http://www.google.com" );
+            SplashBrowser.Url = new Uri("https://bitbucket.org/DJDarkViper/visualudk/wiki/Home");
 
             ProjectList.View = View.Details;
             ProjectList.FullRowSelect = true;
